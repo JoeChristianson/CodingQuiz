39f564b712847d4d073b783ccfba0633bcb5ecdf
Joe Christianson <jcfargond@gmail.com>
2022-03-24 17:18:35 -0500

    made timer work

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+var mainSection = document.querySelector("main")
+var startQuizButton = document.querySelector("#start-quiz-btn")
+var timeSpan = document.querySelector("#time-span");
+var timeLeft = 10;
+
+function startQuiz(){
+    console.log("started quiz!")
+    startTimer()
+    mainSection.innerHTML = ""
+}
+
+function startTimer(){
+    var countdown = setInterval(function(){
+        if(timeLeft===0){
+            clearInterval(countdown)
+            endQuiz()
+        }
+        else {
+            timeLeft--;
+            timeSpan.textContent = timeLeft;
+        }
+    },1000)
+}
+
+function endQuiz(){
+    console.log("Time is Up!")
+}
+
+startQuizButton.addEventListener("click",startQuiz)
