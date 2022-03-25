d2a25baeafde7eefdc264955da05ea9cf07b950e
Joe Christianson <jcfargond@gmail.com>
2022-03-24 22:00:33 -0500

    Added score entry form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ var timeLeft = 100;
 var questionIndex = 0;
 var questions = [];
 var points = 0;
+var countdown;
 
 class Question{
     constructor(index,question,correctAnswer,otherAnswers){
@@ -40,7 +41,7 @@ function startQuiz(){
 }
 
 function startTimer(){
-    var countdown = setInterval(function(){
+    countdown = setInterval(function(){
         if(timeLeft===0){
             clearInterval(countdown)
             endQuiz()
@@ -53,12 +54,43 @@ function startTimer(){
 }
 
 function endQuiz(){
-    console.log("Time is Up!")
+    clearInterval(countdown);
+    mainSection.innerHTML = "";
+    var el0 = document.createElement("h3");
+    el0.textContent = "You scored "+points+" Points!"
+    mainSection.append(el0)
+    var el1 = document.createElement("h3");
+    el1.textContent = "Enter three letters for your initials.";
+    mainSection.append(el1)
+    var input = document.createElement("input");
+    mainSection.append(input);
+    var submit = document.createElement("button")
+    mainSection.append(submit);
+    submit.textContent = "SUBMIT"
+    submit.addEventListener("click",()=>{
+        var initials = input.value;
+        if (initials.length !== 3){
+            console.log("invalid entry")
+            return
+        }
+        else{
+            var newScore = {
+                initials:initials,
+                score:points,
+            }
+            var highScores = JSON.parse(localStorage.getItem("highScores"));
+            highScores.push(newScore);
+            localStorage.setItem("highScores",JSON.stringify(highScores));
+            promptPlayAgain();
+        }
+    })
+    
+
 }
 
 function setQuestionValues(question){
     if (question === undefined){
-        console.log("You're Done");
+        endQuiz()
         return;
     }
 
@@ -99,8 +131,20 @@ mainSection.addEventListener("click",function(event){
 
 function answerWrong(){
     document.querySelector("footer").textContent = "WRONG!";
+    setTimeout(()=>{
+        document.querySelector("footer").textContent = "";
+    },3000)
+    timeLeft -=10;
 }
 
 function answerCorrect(){
     document.querySelector("footer").textContent = "CORRECT!";
+    setTimeout(()=>{
+        document.querySelector("footer").textContent = "";
+    },3000)
+    points++;
+}
+
+function promptPlayAgain(){
+    console.log("Play Again?")
 }
